c724d6e1310a0a45544d5ee1d0d4408680d11705
kuenzangdee <kluckyd@gmail.com>
2024-12-11 20:38:01 +0800

    Update streamlit_app.py
    copied from my Custom Smoothies Sis app

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ st.write(
     """Choose the fruits you want in your custom smoothie
     """
 )
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on smoothie will be:", name_on_order)
 
